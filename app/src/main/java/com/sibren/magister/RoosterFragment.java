2333ec5857d20fb21706667b1321610d94101562
Sibren Talens <sibrentalens@gmail.com>
2015-09-23 19:28:17 +0200

    asdf

--- app/src/main/java/com/sibren/magister/RoosterFragment.java
+++ app/src/main/java/com/sibren/magister/RoosterFragment.java
@@ -1,20 +1,14 @@
 package com.sibren.magister;
 
 import android.os.Bundle;
-//import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by jemoeder on 10-9-2015.
- */
-public class RoosterFragment extends Fragment
-{
+public class RoosterFragment extends Fragment{
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rooster, container, false);
     }
