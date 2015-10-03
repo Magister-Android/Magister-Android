3120cd7bc235d920cc7885a4449fe2dbeecafa5a
Max Verbeek <m4xv3rb33k@gmail.com>
2015-10-03 12:41:27 +0200

    Fix imports die niet nodig zijn

--- app/src/main/java/ml/informaticaowners/magister/Main.java
+++ app/src/main/java/ml/informaticaowners/magister/Main.java
@@ -1,14 +1,12 @@
 package ml.informaticaowners.magister;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
