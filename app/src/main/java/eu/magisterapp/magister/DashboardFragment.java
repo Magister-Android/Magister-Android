003198a93dad3acec049ab70ce2841bdb476a87e
Max Verbeek <m4xv3rb33k@gmail.com>
2015-12-07 23:03:09 +0100

    Gebruik DateTime instances, zodat de tijdzone een beetje minder scheel is

--- app/src/main/java/eu/magisterapp/magister/DashboardFragment.java
+++ app/src/main/java/eu/magisterapp/magister/DashboardFragment.java
@@ -15,7 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-import org.joda.time.LocalDateTime;
+import org.joda.time.DateTime;
 import org.json.JSONException;
 
 import java.io.IOException;
